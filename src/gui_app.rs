426389004e6ee6acaa0a333b9a7616c5ed91e50d
pubnoconst <noconst@emai.com>
2023-03-22 21:15:53 +1100

    init gui

--- src/gui_app.rs
+++ src/gui_app.rs
@@ -1,3 +1,72 @@
+#![allow(non_snake_case)]
+use dioxus::prelude::*;
+
 pub fn run() {
-    println!("Hello from the gui");
+    hot_reload_init!();
+    dioxus_desktop::launch(App);
+}
+
+fn App(cx: Scope) -> Element {
+    cx.render(rsx! {
+        style { include_str!("style.css") },
+        div {
+            id: "MainCard",
+            class: "FlexContainer",
+            p {
+                id: "Greet",
+                "Hello and Welcome to ER Mule Copier",
+            },
+            div {
+                id: "IOCard",
+                class: "FlexContainer",
+                div {
+                    id: "SourceCard",
+                    class: "FlexContainer",
+                    button {
+                        "Browse source"
+                    },
+                    select {
+                        option {
+                            value: "slot 1",
+                            "slot 1"
+                        },
+                        option {
+                            value: "slot 2",
+                            "slot 2"
+                        },
+                        option {
+                            value: "slot 3",
+                            "slot 3"
+                        },
+                    },
+                },
+                div {
+                    id: "TargetCard",
+                    class: "FlexContainer",
+                    button {
+                        "Browse target"
+                    },
+                    select {
+                        option {
+                            value: "slot 1",
+                            "slot 1"
+                        },
+                        option {
+                            value: "slot 2",
+                            "slot 2"
+                        },
+                        option {
+                            value: "slot 3",
+                            "slot 3"
+                        },
+                    }
+                }
+            }
+            button {
+                id: "CopyButton",
+                "Copy"
+            }
+
+         }
+    })
 }
